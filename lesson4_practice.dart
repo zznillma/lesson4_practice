8b923e3faa1ee7ab0c31d8cdf061fe6163119c29
Bektur <zznillma@MacBook-Pro-Bektur.local>
2022-11-23 22:17:35 +0600

    lesson4_practice

---
+++ lesson4_practice.dart
@@ -0,0 +1,67 @@
+void main() {
+  // task #1
+  int a = 10;
+  if (a == 10) {
+    print('Верно');
+  } else {
+    print('неверно');
+  }
+
+  // task #2
+  if (a == 0) {
+    print('Верно');
+  } else {
+    print('неверно');
+  }
+
+  // task #3
+  if (a > 0) {
+    print('Верно');
+  } else {
+    print('неверно');
+  }
+
+  // task #4
+  if (a < 0) {
+    print('Верно');
+  } else {
+    print('неверно');
+  }
+
+  // task #5
+  if (a >= 0) {
+    print('Верно');
+  } else {
+    print('неверно');
+  }
+
+  // task #6
+  if (a <= 0) {
+    print('Верно');
+  } else {
+    print('неверно');
+  }
+
+  // task $7
+  if (a != 0) {
+    print('Верно');
+  } else {
+    print('неверно');
+  }
+
+  // task #8
+  String a1 = 'test';
+  if (a1 == 'test') {
+    print('Верно');
+  } else {
+    print('неверно');
+  }
+
+  // task #9
+  var a2 = '1';
+  if (a2 == '1' && a2.runtimeType == String) {
+    print('Верно');
+  } else {
+    print('неверно');
+  }
+}
